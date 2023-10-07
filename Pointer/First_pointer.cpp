d288da110996703c3b4b2dbf4d2f74247d484cf8
JobayerFaisal <jobayer.faisal.edu@gmail.com>
2023-10-07 20:08:11 +0600

    Added example

--- Pointer/First_pointer.cpp
+++ Pointer/First_pointer.cpp
@@ -1,9 +1,24 @@
 // Your First C++ Program
 
-#include <iostream>
+
+#include <bits/stdc++.h>
+using namespace std; 
 
 int main() {
-    std::cout << "Hello World!";
+
+    int a, b,c ;
+    int *p = 0;  // It is better to set the pointer value NULL. It helps 
+    cout << " Enter Numbers : ";
+     cin >> a >> b ;
+
+    c = a+b ;
+     p = &c ; 
+
+    cout<<"The Sum is :" << *p << endl;
+    cout<<"The Address of Sum :" << p <<endl;
+    cout<<"The Next Address of Sum is :" << p+1 <<endl; // The output will show mainly P+4, as it takes integer as input as well as 4 bits.
+
     return 0;
+
 }
 
--- Pointer/hello.cpp
+++ Pointer/hello.cpp
@@ -1,6 +1,18 @@
 #include <iostream>
+using namespace std; 
 
 int main() {
-    std::cout << "Hello World!";
+   cout << "Hello World! "<< endl;
+
+
+  int x;
+  cout << "Type a number: "; // Type a number and press enter
+  cin >> x; // Get user input from the keyboard
+  cout << "Your number is: " << x;
+
+
+
+
+
     return 0;
 }
